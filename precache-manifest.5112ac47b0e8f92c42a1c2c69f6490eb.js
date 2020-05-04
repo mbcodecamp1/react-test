a1b645be5f768a3c915d7df44e4346a31949fd46
Markus Botzenhart <mbcodecamp1@gmail.com>
2020-05-04 17:01:38 +0200

    Updates

---
+++ precache-manifest.5112ac47b0e8f92c42a1c2c69f6490eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad146ec3dbf762b99f2cf2a0b6622d9",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "63c58be4902a0db258aa",
+    "url": "/react-test/static/css/main.d2f65186.chunk.css"
+  },
+  {
+    "revision": "2d112f72e3fe2c1f9edd",
+    "url": "/react-test/static/js/2.8ea28179.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.8ea28179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63c58be4902a0db258aa",
+    "url": "/react-test/static/js/main.c49e969c.chunk.js"
+  },
+  {
+    "revision": "7e000cb756006e09cc06",
+    "url": "/react-test/static/js/runtime-main.d3c5cd74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
